5b9013fa0ae1ada3ef7f2c95420f97846be6c084
LucilePereira <123165101+LucilePereira@users.noreply.github.com>
2024-12-30 02:01:15 +0100

    Mise à jour de searchQuestion.js : réparer les issues #2, #4, #13, #16 et #17

--- libs/searchQuestion.js
+++ libs/searchQuestion.js
@@ -1,20 +1,19 @@
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
 const fs = require("fs");
 const path = require("path");
 const afficheQuestion = require("./afficheQuestion.js");
 const getCategorieQuestion = require("./categorieQuestion.js");
+const {openDirectory} = require('./askAndOpen.js');
+const {openFile} = require('./askAndOpen.js');
+const {askQuestion} = require('./askAndOpen.js');
+const {askAndTryFile} = require('./askAndOpen.js');
 
 function getQuestionsWkeyword(directory, keywords) {
   var questionsWKeywords = [];
-  fs.readdirSync(directory).forEach((file) => {
+  openDirectory(directory).forEach((file) => {
     const fullPath = path.join(directory, file);
     if (fs.lstatSync(fullPath).isFile()) {
       // Si c'est un fichier, lire et afficher le contenu
-      const content = fs.readFileSync(fullPath, "utf8");
+      const content = openFile(fullPath);
       const questions = content.split("\n\n");
       let isNewExercise = true;
       let consigne = "";
@@ -46,10 +45,10 @@ function getQuestionsWkeyword(directory, keywords) {
 }
 function getQuestionWCateg(dir, categs) {
   var questionsWCateg = [];
-  fs.readdirSync(dir).forEach((file) => {
+  openDirectory(dir).forEach((file) => {
     const fullPath = path.join(dir, file);
     if (fs.lstatSync(fullPath).isFile()) {
-      const content = fs.readFileSync(fullPath, "utf8");
+      const content = openFile(fullPath);
       const questions = content.split("\n\n");
       let isNewExercise = true;
       let consigne = "";
@@ -76,77 +75,45 @@ function getQuestionWCateg(dir, categs) {
   return questionsWCateg;
 }
 async function addQuestion(questions) {
-  let nomExam = await new Promise((resolve) => {
-    readline.question(
-      "\nNom de l'examen auquel ajouter une de ces question : ",
-      resolve
-    );
-  });
-  let numQuest = parseInt(
-    await new Promise((resolve) => {
-      readline.question("\nNuméro de la question à ajouter : ", resolve);
-    }),
-    10
-  );
-  addQuestionToFile(nomExam,numQuest,questions)
-}
-
-async function addQuestionToFile(nomExam,numQuest,questions){
-  let isWritten = false;
-  fs.readdirSync("./examens").forEach((file) => {
-    if (file === nomExam + ".gift") {
-      fs.appendFileSync(
-        path.join("./examens", file),
-        questions[numQuest] + "\n\n",
-        "utf8"
-      );
-      isWritten = true;
-      console.log("Fichier modifié !");
+  let exam = await askAndTryFile ("\nAdresse de l'examen auquel ajouter une de ces question : ")
+  let numQuest = parseInt(await askQuestion("\nNuméro de la question à ajouter : "), 10);
+  
+    if (exam.exists) {
+        fs.appendFileSync(
+            "./" + exam.path,
+            questions[numQuest] + "\n\n",
+            "utf8"
+        );
+        console.log("Fichier modifié !");
     }
-  });
-  if (!isWritten) {
-    try {
-      await fs.promises.writeFile(
-        "./examens/" + nomExam + ".gift",
-        questions[numQuest] + "\n\n",
-        "utf8"
-      );
-      console.log("Fichier créé avec succès");
-    } catch (err) {
-      console.error("Erreur lors de l'écriture du fichier :", err);
+    else {
+        try {
+            await fs.promises.writeFile(
+                "./" + exam.path,
+                questions[numQuest] + "\n\n",
+                "utf8"
+            );
+            console.log("Fichier créé avec succès");
+        } catch (err) {
+            console.error("Erreur lors de l'écriture du fichier :", err);
+        }
     }
-  }
 }
 
 async function searchAddQuestion() {
   let reponse = "";
   let questionsSelected;
   while (reponse != "1" && reponse != "2") {
-    reponse = await new Promise((resolve) => {
-      readline.question(
-        "\nRechercher par mot clés(1) ou par type de questions(2) : ",
-        resolve
-      );
-    });
+    reponse = await askQuestion("\nRechercher par mot clés(1) ou par type de questions(2) : ");
   }
   if (reponse === "1") {
-    reponse = await new Promise((resolve) => {
-      readline.question(
-        "\nMots clé à rechercher, séparés par une virgule : ",
-        resolve
-      );
-    });
+    reponse = await askQuestion("\nMots clé à rechercher, séparés par une virgule : ");
     questionsSelected = getQuestionsWkeyword(
       "./SujetB_data",
       reponse.split(",")
     );
   } else {
-    reponse = await new Promise((resolve) => {
-      readline.question(
-        "\nType de question à rechercher : question ouverte(1), question fermée(2) ",
-        resolve
-      );
-    });
+    reponse = await askQuestion("\nType de question à rechercher : question ouverte(1), question fermée(2) ");
     if (reponse === "1") {
       questionsSelected = getQuestionWCateg("./SujetB_data", [
         "mot manquant",
@@ -164,4 +131,4 @@ async function searchAddQuestion() {
   await addQuestion(questionsSelected);
 }
 
-module.exports = { searchAddQuestion, getQuestionsWkeyword, getQuestionWCateg, addQuestionToFile };
+module.exports = { searchAddQuestion, getQuestionsWkeyword, getQuestionWCateg, addQuestion };
